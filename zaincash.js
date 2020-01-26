8c51a6e5a96fd04ac5e465e97693040a69693c36
hamdon <hamdongunner@gmail.com>
2020-01-26 19:06:44 +0300

    ✨ Some Fixes

--- zaincash.js
+++ zaincash.js
@@ -1,8 +1,18 @@
-const jwt = require('jsonwebtoken');
-const request = require('request');
+const jwt = require("jsonwebtoken");
+const request = require("request");
 
 class Zaincash {
-  constructor({amount, orderId, serviceType, redirectUrl, production, msisdn, merchantId, secret, lang}) {
+  constructor({
+    amount,
+    orderId,
+    serviceType,
+    redirectUrl,
+    production,
+    msisdn,
+    merchantId,
+    secret,
+    lang
+  }) {
     this.amount = amount;
     this.serviceType = serviceType;
     this.redirectUrl = redirectUrl;
@@ -11,58 +21,58 @@ class Zaincash {
     this.secret = secret;
     this.lang = lang;
     this.time = Date.now();
-    if(production){
-      this.initUrl = 'https://api.zaincash.iq/transaction/init';
-      this.requestUrl = 'https://api.zaincash.iq/transaction/pay?id=';
-    }else {
-      this.initUrl = 'https://test.zaincash.iq/transaction/init';
-      this.requestUrl = 'https://test.zaincash.iq/transaction/pay?id=';
+    if (production) {
+      this.initUrl = "https://api.zaincash.iq/transaction/init";
+      this.requestUrl = "https://api.zaincash.iq/transaction/pay?id=";
+    } else {
+      this.initUrl = "https://test.zaincash.iq/transaction/init";
+      this.requestUrl = "https://test.zaincash.iq/transaction/pay?id=";
     }
   }
 
-  init(){
+  init() {
     const data = {
-      'amount': this.amount,
-      'serviceType': this.serviceType,
-      'msisdn': this.msisdn,
-      'orderId': this.orderId,
-      'redirectUrl': this.redirectUrl,
-      'iat': this.time,
-      'exp': this.time + 60 * 60 * 4
+      amount: this.amount,
+      serviceType: this.serviceType,
+      msisdn: this.msisdn,
+      orderId: this.orderId,
+      redirectUrl: this.redirectUrl,
+      iat: this.time,
+      exp: this.time + 60 * 60 * 4
     };
 
     const token = jwt.sign(data, this.secret);
 
     const postData = {
-      'token': token,
-      'merchantId': this.merchantId,
-      'lang': this.lang
+      token: token,
+      merchantId: this.merchantId,
+      lang: this.lang
     };
 
     const requestOptions = {
       uri: this.initUrl,
       body: JSON.stringify(postData),
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       }
     };
-     return new Promise(function(resolve, reject) {
-       request(requestOptions, function (error, response) {
-         if(JSON.parse(response.body).id)
-          resolve (JSON.parse(response.body).id);
-          reject (response.body);
-       });
+    return new Promise(function(resolve, reject) {
+      request(requestOptions, function(error, response) {
+        if (error) reject(error);
+        if (response && response.body && JSON.parse(response.body).id)
+          resolve(JSON.parse(response.body).id);
+        reject(response.body);
+      });
     });
   }
 
-  pay(transactionId, res){
+  pay(transactionId, res) {
     res.writeHead(302, {
-        'Location': this.requestUrl + transactionId
-      });
+      Location: this.requestUrl + transactionId
+    });
     res.end();
   }
-
 }
 
 module.exports = Zaincash;
